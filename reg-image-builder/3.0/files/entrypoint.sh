42e8fc58110cd26579a1af3c950e437a6db2b07e
htolimat <htolimat>
2025-12-29 14:48:38 +0100

    testing with other name

--- reg-image-builder/3.0/files/entrypoint.sh
+++ reg-image-builder/3.0/files/entrypoint.sh
@@ -179,13 +179,13 @@ main()
         if [[ "${CHECK_BEFORE_PUSH:-}" ]];
         then
             # Define some useful variables
-            local local_image_oci=${TAG##*/}_oci.tar
+            local local_image_oci=image_oci.tar
             local output="type=oci,name=$TAG,dest=$local_image_oci"
             
             # Build image without pushing it
             build_image $output
 
-            local local_image=docker_image.tar
+            local local_image=image_docker.tar
             skopeo copy oci-archive:$local_image_oci docker-archive:$local_image
 
             # Compare built image with existing image
