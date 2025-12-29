997b2d2a000244baf51fc3d345d5e0dc69cdc5c0
htolimat <htolimat>
2025-12-29 14:33:00 +0100

    test new name for local image

--- reg-image-builder/3.0/files/entrypoint.sh
+++ reg-image-builder/3.0/files/entrypoint.sh
@@ -185,7 +185,7 @@ main()
             # Build image without pushing it
             build_image $output
 
-            local local_image=${TAG##*/}.tar
+            local local_image=${TAG##*/}_docker.tar
             skopeo copy oci-archive:$local_image_oci docker-archive:$local_image
 
             # Compare built image with existing image
